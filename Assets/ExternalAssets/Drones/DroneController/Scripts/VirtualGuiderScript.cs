8105cc3436d41d6e0b21b8d99bce1b0d880cce06
kelbin <kaeltero@gmail.com>
2021-12-03 17:39:46 +0300

    statistics

---
+++ Assets/ExternalAssets/Drones/DroneController/Scripts/VirtualGuiderScript.cs
@@ -0,0 +1,16 @@
+using Crosstales.RTVoice;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Crosstales.RTVoice;
+using Crosstales.RTVoice.Tool;
+using System;
+
+public class VirtualGuiderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+}
---
+++ Assets/GetStatistics.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GetStatistics : MonoBehaviour
+{
+    public GameObject drone;
+    public int charges;
+
+    public TextMeshProUGUI chargesLabel;
+    public TextMeshProUGUI heightDroneLabel;
+    public TextMeshProUGUI coordinatesLabel;
+
+    RaycastHit hit;
+    float heightAboveGround = 0;
+
+    void Start()
+    {
+        
+    }
+
+    public IEnumerator ChangeChargesDrone()
+    {
+        yield return new WaitForSeconds(90);
+        charges -= 1;
+        chargesLabel.text = charges.ToString();
+    }
+
+    void Update()
+    {
+        ChangeChargesDrone();
+        coordinatesLabel.text = "Координата: " + drone.transform.position.x + "," + drone.transform.position.y;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(drone.transform.position, drone.transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity))
+        {
+            heightDroneLabel.text = "Высота: " + hit.distance.ToString() + " м";
+        }
+    }
+}
