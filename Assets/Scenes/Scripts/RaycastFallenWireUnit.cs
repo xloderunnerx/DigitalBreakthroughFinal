024b446b9fb42d5d5922dbab0a23d77c7ba3235f
Glav Mraz <xloderunnerx@gmail.com>
2021-12-03 23:14:37 +0300

    Exam scene.

---
+++ Assets/Scenes/Scripts/RaycastFallenWireUnit.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastFallenWireUnit : MonoBehaviour
+{
+    public GameObject buttonHintPrefab;
+    public GameObject buttonHint;
+    public FallenWireUnit locked;
+    public string warning;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (buttonHint != null)
+        {
+            buttonHint.transform.LookAt(new Vector3(Camera.main.transform.position.x, buttonHint.transform.position.y, Camera.main.transform.position.z));
+        }
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.green, 0.1f);
+        var hit = new RaycastHit();
+        Physics.Raycast(transform.position, transform.forward, out hit, 10);
+        if (hit.collider == null)
+            return;
+
+        if (hit.collider.GetComponent<FallenWireUnit>() == null)
+        {
+            locked = null;
+            Destroy(buttonHint);
+            return;
+        }
+
+        var powerUnit = hit.collider.GetComponent<FallenWireUnit>();
+
+        if (powerUnit.collected)
+        {
+            locked = null;
+            Destroy(buttonHint);
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            powerUnit.collected = true;
+        }
+
+        if (locked != powerUnit)
+        {
+            locked = null;
+            Destroy(buttonHint);
+        }
+        locked = powerUnit;
+        if (buttonHint == null)
+        {
+            buttonHint = Instantiate(buttonHintPrefab);
+            buttonHint.transform.position = powerUnit.transform.position + new Vector3(0, powerUnit.GetComponent<CapsuleCollider>().bounds.size.y * 1.25f, 0);
+        }
+
+    }
+}
